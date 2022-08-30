2483be7466e6837db37cfe52306ed34f2e2c14a7
Tin <Tin@email>
2022-08-30 14:47:17 -0700

    Changed image gallery size

--- components/Gallery.js
+++ components/Gallery.js
@@ -15,9 +15,10 @@ const Gallery = ({ images }) => {
           <div className='flex gap-4 flex-wrap justify-between'>
               {images.map(image => (
                 <div onClick={() => changeImage(image.image.asset.url)} key={image._id} 
-                className='relative w-40 h-40 md:w-64 md:h-64 cursor-pointer'>
+                className='relative w-full h-40 md:w-64 md:h-64 cursor-pointer'>
                     <Image 
                       src={image?.image.asset.url} 
+                      width='100%'
                       layout='fill' 
                       objectFit='cover' 
                       alt='' />
